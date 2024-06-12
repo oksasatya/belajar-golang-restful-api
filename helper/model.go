3d1f9972554e6d0a6aa923103c498c9b0d96437f
oksa satya <78420053+oksasatya@users.noreply.github.com>
2024-06-12 14:41:24 +0400

    Study Golang Restful APi

---
+++ helper/model.go
@@ -0,0 +1,21 @@
+package helper
+
+import (
+	"belajar-golang-restful-api/model/domain"
+	"belajar-golang-restful-api/model/web"
+)
+
+func ToCategoryResponse(category domain.Category) web.CategoryResponse {
+	return web.CategoryResponse{
+		Id:   category.Id,
+		Name: category.Name,
+	}
+}
+
+func ToCategoryResponses(categories []domain.Category) []web.CategoryResponse {
+	var categoriesResponses []web.CategoryResponse
+	for _, category := range categories {
+		categoriesResponses = append(categoriesResponses, ToCategoryResponse(category))
+	}
+	return categoriesResponses
+}
