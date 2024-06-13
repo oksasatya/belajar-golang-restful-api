2da89c7f95f4d72c1bf05b3bde123d0638a3c39c
oksa satya <78420053+oksasatya@users.noreply.github.com>
2024-06-13 13:26:49 +0400

    add injector

---
+++ simple/simple.go
@@ -0,0 +1,27 @@
+package simple
+
+import (
+	"errors"
+)
+
+type SimpleRepository struct {
+	Error bool
+}
+
+func NewSimpleRepository() *SimpleRepository {
+	return &SimpleRepository{}
+}
+
+type SimpleService struct {
+	*SimpleRepository
+}
+
+func NewSimpleService(repository *SimpleRepository) (*SimpleService, error) {
+	if repository.Error {
+		return nil, errors.New("Failed create Servoce")
+	} else {
+		return &SimpleService{
+			SimpleRepository: repository,
+		}, nil
+	}
+}
